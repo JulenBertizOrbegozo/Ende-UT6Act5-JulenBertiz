ba8c99378537cf0df2586f671876aaa99e5faeae
JulenBertizOrbegozo <jvertizorb1@educacion.navarra.es>
2025-04-10 10:42:38 +0200

    Before hecho
    Ya esta hecho que antes de cada uno cree un usuario

---
+++ Ende Cuenta/src/CuentaTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.BeforeEach;
+
+class CuentaTest {
+
+    @BeforeEach
+    public void nuevaCuenta() {
+        Cuenta cuentaPedro = new Cuenta("ES20", 100);
+    }
+    
+}
